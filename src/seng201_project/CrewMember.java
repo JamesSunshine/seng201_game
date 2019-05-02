4b53bda5ac4c3b8c75e5d3f83779a7d6ef71a1a6
James Christopher Sunshine <jsu85@cs18237kq.canterbury.ac.nz>
2019-05-02 23:17:34 +1200

    added crewType class

--- src/seng201_project/CrewMember.java
+++ src/seng201_project/CrewMember.java
@@ -2,6 +2,8 @@ package seng201_project;
 
 public class CrewMember {
 
-    
+    public String type;
+    public int hunger = 10;
+    public int tiredness = 10;
 
 }
---
+++ src/seng201_project/crewType.java
@@ -0,0 +1,10 @@
+package seng201_project;
+
+public class crewType {
+
+        public String typeString;
+        public int hungerChange;
+        public int tirednessChange;
+        public String specSkill;
+        public int cost;
+}
