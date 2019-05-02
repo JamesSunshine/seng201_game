bffc7f2e3325d708d4aac370b10732bbb01b7719
James Christopher Sunshine <jsu85@cs18237kq.canterbury.ac.nz>
2019-05-02 23:40:33 +1200

    added constructors for crewtype

--- src/seng201_project/CrewType.java
+++ src/seng201_project/CrewType.java
@@ -12,7 +12,53 @@ public class CrewType {
 
         public static void main(String[] args) {
 
-            CrewType engineer = new CrewType();
-            engineer.ty
+            CrewType engineer = new CrewType();     //engineer crew type
+            engineer.typeString = "engineer";
+            engineer.hp = 15;
+            engineer.hungerChange = 2;
+            engineer.tirednessChange = 2;
+            engineer.specSkill = "engineer";
+            engineer.cost = 5;
+
+            CrewType pilot = new CrewType();       //pilot crew type
+            pilot.typeString = "pilot";
+            pilot.hp = 10;
+            pilot.hungerChange = 2;
+            pilot.tirednessChange = 2;
+            pilot.specSkill = "pilot";
+            pilot.cost = 3;
+
+            CrewType medic = new CrewType();
+            medic.typeString = "medic";
+            medic.hp = 15;
+            medic.hungerChange = 2;
+            medic.tirednessChange = 2;
+            medic.specSkill = "medic";
+            medic.cost = 4;
+
+            CrewType scout = new CrewType();
+            scout.typeString = "scout";
+            scout.hp = 15;
+            scout.hungerChange = 2;
+            scout.tirednessChange = 2;
+            scout.specSkill = "scout";
+            scout.cost = 6;
+
+            CrewType grunt = new CrewType();
+            grunt.typeString = "grunt";
+            grunt.hp = 15;
+            grunt.hungerChange = 1;
+            grunt.tirednessChange = 1;
+            grunt.specSkill = "grunt";
+            grunt.cost = 1;
+
+            CrewType gunner = new CrewType();
+            gunner.typeString = "gunner";
+            gunner.hp = 15;
+            gunner.hungerChange = 2;
+            gunner.tirednessChange = 1;
+            gunner.specSkill = "gunner";
+            gunner.cost = 2;
+
         }
 }
